234acc0b471d58088f1dd006629ec2dd59193e94
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2023-01-02 07:07:48 +0100

    add Nav and Homescreen component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import HomeScreen from "./HomeScreen";
 import "./App.css";
 
 function App() {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <HomeScreen />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HomeScreen.css";
+
+export default function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+      {/* Navbar */}
+
+      {/* Banner */}
+
+      {/* Row */}
+    </div>
+  );
+}
