9d398f399ddea17e8e551aca160c99cfac3d40c5
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2023-01-02 06:35:28 +0100

    setup basic structure

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDoMEVXaJz1FxPKCM2cuy-E_7cw7YIEDE8",
+  authDomain: "netflix-clone-31fa3.firebaseapp.com",
+  projectId: "netflix-clone-31fa3",
+  storageBucket: "netflix-clone-31fa3.appspot.com",
+  messagingSenderId: "212513076630",
+  appId: "1:212513076630:web:77928e25a46c8d51b1d9b7",
+};
