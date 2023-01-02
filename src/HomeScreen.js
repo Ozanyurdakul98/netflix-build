d36318e2397dd41f7c2f389ac43a0126c6af818e
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2023-01-02 13:42:31 +0100

    edit navbar and add banner image

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./HomeScreen.css";
+import Nav from "./Nav";
 
 export default function HomeScreen() {
   return (
     <div className='homeScreen'>
       {/* Navbar */}
-
+      <Nav />
       {/* Banner */}
 
       {/* Row */}
