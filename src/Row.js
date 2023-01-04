90611b706690c2cf2ff242aebe64af7e8070fd90
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2023-01-04 15:20:16 +0100

    add row component and fetching movie data into it

---
+++ src/Row.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+import axios from "./axios";
+
+function Row({ title, fetchURL, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+  const baseURL = "https://image.tmdb.org/t/p/original/";
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+  return (
+    <div className='row'>
+      <h2>{title}</h2>
+      <div className='row_posters'>
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                key={movie.id}
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
